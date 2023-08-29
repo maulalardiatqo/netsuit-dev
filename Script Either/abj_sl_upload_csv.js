dc031447152367c032568d4fa6c7f96d7c152b7b
MaulalArdi <maulalardiatqo123@gmail.com>
2023-08-29 14:56:31 +0700

    update abj_sl_upload_csv

--- Script Either/abj_sl_upload_csv.js
+++ Script Either/abj_sl_upload_csv.js
@@ -40,8 +40,8 @@ define(['N/ui/serverWidget', 'N/file', 'N/record', 'N/format', 'N/search'], func
                         lines.forEach(function(line, index) {
                             if (index > 0) {
                                 var values = line.split(',');
-                                if (values.length >= 14) {
-                                    var subsidiary = 2
+                                if (values.length >= 15) {
+                                    var subsidiary = 12
                                     var tranid = values[2];
                                     var trandate = values[3];
                                     var idAccount = values[6];
@@ -49,44 +49,17 @@ define(['N/ui/serverWidget', 'N/file', 'N/record', 'N/format', 'N/search'], func
                                     var memo = values[10]
                                     var memoLine = values[11];
                                     var amount = values[14];
+                                    var periodId = values[15].replace(/\r/g, '');
                                     log.debug('trandate atas', trandate);
                                     if(trandate){
-                                        var trandateToConve = trandate;
-                                        trandate = format.parse({value:trandateToConve, type: format.Type.DATE});
-                                        
-                                        var parts = trandateToConve.split("/");
-                                        var month = parseInt(parts[0]);
-                                        var day = parseInt(parts[1]);
-                                        var year = parseInt(parts[2]);
-                                        var dateObject = new Date(year, month - 1, day);
-                                        var monthIndex = dateObject.getMonth();
-                                        var yearIndex = dateObject.getFullYear();
-                                        log.debug('monthIndex', monthIndex);
-                                        
-                                        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-                                        var periodMonth = months[monthIndex];
-                                        var periodYear = yearIndex
-                                        var postingPeriod = periodMonth + ' ' + periodYear
-                                        log.debug('postingPeriod', postingPeriod);
-                                        log.debug('periodMonth', periodMonth);
-                                        var searchPeriod = search.load({
-                                            id : 'customsearch_acounting_period'
-                                        });
-                                        searchPeriod.filters.push(search.createFilter({
-                                            name: 'periodname',
-                                            operator: search.Operator.IS,
-                                            values: postingPeriod
-                                        }));
-                                        var postingPeriodDataSet = searchPeriod.run();
-                                        searchPeriod = postingPeriodDataSet.getRange(0, 1);
-                                        var periodId = searchPeriod[0].getValue('internalid');
-                                        log.debug('periodid', periodId);
+                                        trandate = format.parse({value:trandate, type: format.Type.DATE});
                                     }
                                     
-                                    
+                                    log.debug('data', {'subsidiary' : subsidiary, 'tranid' : tranid, 'trandate' : trandate, 'idAccount' : idAccount, 'accountdepid' : accountDepID, 'memo' : memo, 'memoLine' : memoLine, 'amount' : amount, 'periodId' : periodId});
                                     if (idAccount && tranid) {
                                         if (!transactions[tranid]) {
                                             transactions[tranid] = {
+                                                tranid : tranid,
                                                 idaccount: idAccount,
                                                 trandate: trandate,
                                                 accountDepID: accountDepID,
@@ -98,17 +71,18 @@ define(['N/ui/serverWidget', 'N/file', 'N/record', 'N/format', 'N/search'], func
                                         }
                                         
                                         var lineData = {
-                                            memo: memoLine,
+                                            memoLine: memoLine,
                                             amount: amount,
                                             accountDepID
                                         };
-
+                                        log.debug('lineData', lineData)
                                         transactions[tranid].lines.push(lineData);
                                     }
                                 }
                             }
                         });
                         var successRec = 0;
+                        var recIDArry = []
 
                         for (var tranid in transactions) {
                             var transaction = transactions[tranid];
@@ -122,32 +96,37 @@ define(['N/ui/serverWidget', 'N/file', 'N/record', 'N/format', 'N/search'], func
                                 value: transaction.idaccount,
                                 ignoreFieldChange: true
                             });
+                            log.debug('setAccount',transaction.idaccount)
                             newRecord.setValue({
                                 fieldId: 'tranid',
                                 value: transaction.tranid,
                                 ignoreFieldChange: true
                             });
+                            log.debug('setTranid', transaction.tranid)
                             newRecord.setValue({
                                 fieldId: 'trandate',
                                 value: transaction.trandate,
                                 ignoreFieldChange: true
                             });
+                            log.debug('setTrandate', transaction.trandate)
                             newRecord.setValue({
                                 fieldId: 'postingperiod',
                                 value: transaction.periodId,
                                 ignoreFieldChange: true
                             });
+                            log.debug('setPostingPeriod', transaction.periodId)
                             newRecord.setValue({
                                 fieldId: 'subsidiary',
                                 value: transaction.subsidiary,
                                 ignoreFieldChange: true
                             });
+                            log.debug('setSubsidiary', transaction.subsidiary)
                             newRecord.setValue({
                                 fieldId: 'memo',
                                 value: transaction.memo,
                                 ignoreFieldChange: true
                             });
-                            
+                            log.debug('setMemo', transaction.memo)
                             transaction.lines.forEach(function (lineData) {
                                 newRecord.selectNewLine({
                                     sublistId: 'other'
@@ -158,30 +137,35 @@ define(['N/ui/serverWidget', 'N/file', 'N/record', 'N/format', 'N/search'], func
                                     fieldId: 'account',
                                     value: lineData.accountDepID
                                 });
+                                log.debug('setAccountLine', lineData.accountDepID)
                                 newRecord.setCurrentSublistValue({
                                     sublistId: 'other',
                                     fieldId: 'memo',
-                                    value: lineData.memo
+                                    value: lineData.memoLine
                                 });
+                                log.debug('setMemoLine', lineData.memoLine)
                                 newRecord.setCurrentSublistValue({
                                     sublistId: 'other',
                                     fieldId: 'amount',
                                     value: lineData.amount
                                 });
-
+                                log.debug('setAmount', lineData.amount)
                                 newRecord.commitLine({
                                     sublistId: 'other'
                                 });
+                                log.debug('setCommitLine')
                             });
 
                             var recordId = newRecord.save();
                             log.debug('New record created with ID:', recordId);
                             if(recordId){
                                 successRec++;
+                                recIDArry.push(recordId)
                             }
                             
                         }
-                        context.response.write('<h2>Success</h2><p>import ' + successRec + 'record.</p>');
+                        log.debug('recIDArry', recIDArry);
+                        context.response.write('<h2>Success</h2><p>import ' + successRec + 'record with ID ['+ recIDArry.join(', ') +']</p>');
                     } catch (e) {
                         log.error('Error processing file:', e);
                     }
