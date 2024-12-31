7c3a3e717c8ca5de4acf0b51770caed8e1ad6b49
maulalardiatqo <maulalardiatqo123@gmail.com>
2024-12-31 14:08:47 +0700

    update 31/12/2024

--- Demo/Connect Iseller/abj_sl_create_cash_sale.js
+++ Demo/Connect Iseller/abj_sl_create_cash_sale.js
@@ -9,19 +9,18 @@ define(['N/log', 'N/http', 'N/record', 'N/crypto', 'N/error'], function(log, htt
             try {
                 // Membaca body request POST dari iseller
                 var requestBody = context.request.body;
+                var requestHeader = context.request.headers
                 log.debug('requestBody', requestBody)
+                log.debug('requestHeader', requestHeader)
                 // Timestamp dan APIKey dari header request iseller
                 var timestamp = context.request.headers['Timestamp'];
                 var apiKey = context.request.headers['APIKey'];
                 var signature = context.request.headers['Signature'];
+                var customRecord = record.create({
+                    type: 'customrecord_cs_iseller'
+                });
 
-                // Verifikasi signature
-                if (!isValidSignature(apiKey, requestBody, timestamp, signature)) {
-                    throw new Error('Invalid Signature');
-                }
-
-                // Jika signature valid, lanjutkan proses lainnya
-                log.debug('Valid Request', 'Signature is valid');
+                // Jika signature valid, lanjutkan proses lainny
 
                 // Proses data sesuai kebutuhan
                 // Misalnya, simpan data ke record NetSuite
@@ -56,20 +55,6 @@ define(['N/log', 'N/http', 'N/record', 'N/crypto', 'N/error'], function(log, htt
         }
     }
 
-    // Fungsi untuk memverifikasi signature
-    function isValidSignature(apiKey, jsonBody, timestamp, receivedSignature) {
-        var inputString = apiKey + jsonBody + timestamp;
-        
-        // Perbaikan: Menyediakan algoritma hashing SHA-256
-        var hash = crypto.createHash({ algorithm: crypto.HashAlg.SHA_256 });
-        hash.update(inputString);
-        
-        // Mendapatkan hasil hash dan mengubahnya menjadi format hex
-        var calculatedSignature = hash.digest().toString('hex').toUpperCase();
-    
-        // Bandingkan signature yang diterima dengan signature yang dihitung
-        return calculatedSignature === receivedSignature;
-    }
 
     return {
         onRequest: onRequest
