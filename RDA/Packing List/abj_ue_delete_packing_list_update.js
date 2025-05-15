be48b9b7b5f425918474c71339359ee0e5ad7af7
maulalardiatqo <maulalardiatqo123@gmail.com>
2025-05-15 21:44:36 +0700

    update 15/5/2025

--- RDA/Packing List/abj_ue_delete_packing_list_update.js
+++ RDA/Packing List/abj_ue_delete_packing_list_update.js
@@ -13,17 +13,17 @@ define(["N/record", "N/search", "N/ui/serverWidget", "N/runtime", "N/currency", 
                 var allIdFul = dataRec.getValue('custbody_rda_packlist_do_number');
                 log.debug('allIdFul', allIdFul)
                 log.debug('allIdFul length', allIdFul.length)
+                log.debug('dataRecID', dataRecID)
                 if(allIdFul.length > 100){
                     let params = {
                         custscript_id_item_fulfill_mr: dataRecID,
                         custscript_even_trigger_mr: "delete",
                         custscript_all_id_fulfill_mr : allIdFul
                     };
-        
                     let scriptTask = task.create({
                         taskType: task.TaskType.MAP_REDUCE,
                         scriptId: 'customscript_abj_mr_delete_packing_list', 
-                        deploymentId: 'customdeploy_abj_mr_delete_packing_list',
+                        // deploymentId: 'customdeploy_abj_mr_delete_packing_list',
                         params: params
                     });
         
@@ -32,8 +32,6 @@ define(["N/record", "N/search", "N/ui/serverWidget", "N/runtime", "N/currency", 
                 }else{
                     allIdFul.forEach(function(id) {
                         if (id) {
-                            log.debug('Processing ID', id);
-
                             record.submitFields({
                                 type: "itemfulfillment",
                                 id: id,
@@ -66,17 +64,18 @@ define(["N/record", "N/search", "N/ui/serverWidget", "N/runtime", "N/currency", 
                 var nopol = dataRec.getValue('custbody_rda_packlist_nopol');
                 log.debug('allIdFul', allIdFul)
                 log.debug('allIdFul length', allIdFul.length)
+                log.debug('dataRecID', dataRecID)
                 if(allIdFul.length > 100){
                     let params = {
-                        custscript_id_item_fulfill_mr: dataRecID,
-                        custscript_even_trigger_mr: "create",
+                        custscript_id_item_fulfill_mr : dataRecID,
+                        custscript_even_trigger_mr : "create",
                         custscript_all_id_fulfill_mr : ""
                     };
 
                     let scriptTask = task.create({
                         taskType: task.TaskType.MAP_REDUCE,
                         scriptId: 'customscript_abj_mr_delete_packing_list', 
-                        deploymentId: 'customdeploy_abj_mr_delete_packing_list2',
+                        // deploymentId: 'customdeploy_abj_mr_delete_packing_list2',
                         params: params
                     });
 
@@ -85,8 +84,6 @@ define(["N/record", "N/search", "N/ui/serverWidget", "N/runtime", "N/currency", 
                 }else{
                     allIdFul.forEach(function(id) {
                         if (id) {
-                            log.debug('Processing ID', id);
-
                             record.submitFields({
                                 type: "itemfulfillment",
                                 id: id,
