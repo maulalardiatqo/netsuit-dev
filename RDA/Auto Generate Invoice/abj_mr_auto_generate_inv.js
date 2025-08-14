f60ecb72b3f2c130b801686f8db5dc992785250c
maulal ardi <maulalardiatqo123@gmail.com>
2025-08-14 17:18:58 +0700

    update 14/8/2025

--- RDA/Auto Generate Invoice/abj_mr_auto_generate_inv.js
+++ RDA/Auto Generate Invoice/abj_mr_auto_generate_inv.js
@@ -16,7 +16,7 @@ define(['N/search', 'N/record', 'N/log'], (search, record, log) => {
             searchObj.run().each(result => {
                 results.push(result);
                 count++;
-                return count < 100;
+                return count < 5;
             });
 
             return results;
@@ -29,43 +29,50 @@ define(['N/search', 'N/record', 'N/log'], (search, record, log) => {
     const map = (context) => {
         try {
             const result = JSON.parse(context.value);
-            log.debug('result', result);
-
-            const fulfillmentId = result.values["GROUP(internalid)"]?.[0]?.value; 
-            const soId = result.values["GROUP(appliedToTransaction.internalid)"]?.[0]?.value;
-            const itemId = result.values["GROUP(item.internalid)"]?.[0]?.value;
-            const qty = parseFloat(result.values["MAX(quantity)"]) || 0;
-            const location = result.values["GROUP(location)"]?.[0]?.value; 
+            const values = result.values;
+            log.debug('result', result)
+            const fulfillmentId = values.internalid?.[0]?.value || null;
+            const soId = values["appliedToTransaction.internalid"]?.[0]?.value || null;
+            const itemId = values["item.internalid"]?.[0]?.value || null;
+            const qty = Number(values.quantity || 0);
+            const location = values.location?.[0]?.value || null;
+            const lineIdentifier = values.custcol_rda_line_identifier?.[0]?.value || values.custcol_rda_line_identifier || null;
 
             log.debug('fulfillmentId', fulfillmentId);
             log.debug('soId', soId);
             log.debug('itemId', itemId);
             log.debug('qty', qty);
+            log.debug('location', location);
+            log.debug('lineIdentifier', lineIdentifier);
 
-            if (!fulfillmentId || !soId || !itemId) {
+            if (!fulfillmentId || !soId || !itemId || !qty || !location || !lineIdentifier) {
                 log.error('Data tidak lengkap', result);
                 return;
             }
             context.write({
                 key: fulfillmentId,
-                value: { soId, itemId, qty, location }
+                value: { soId, itemId, qty, location, lineIdentifier }
             });
 
         } catch (e) {
             log.error('Map Error', e);
         }
     };
-
     const reduce = (context) => {
         const fulfillmentId = context.key;
         const linesData = context.values.map(v => JSON.parse(v));
         const soId = linesData[0]?.soId;
         const location = linesData[0]?.location
+        const lineIdentifier = linesData[0]?.lineIdentifier
         log.debug('location', location)
 
         try {
             log.audit(`Proses Fulfillment ID: ${fulfillmentId}`, `SO ID: ${soId}, Total line: ${linesData.length}`);
-
+            const adaQtyMinus = linesData.some(ld => ld.qty < 0);
+            if (adaQtyMinus) {
+                log.audit(`Skip Fulfillment ID ${fulfillmentId}`, `Dilewati karena ada qty negatif`);
+                return; // tidak diproses lebih lanjut
+            }
             const invoiceRec = record.transform({
                 fromType: record.Type.SALES_ORDER,
                 fromId: soId,
@@ -79,29 +86,51 @@ define(['N/search', 'N/record', 'N/log'], (search, record, log) => {
             const lineCount = invoiceRec.getLineCount({ sublistId: 'item' });
             log.debug('lineCount', lineCount);
             for (let i = lineCount - 1; i >= 0; i--) {
-                log.debug('i', i);
-
                 const currentItem = invoiceRec.getSublistValue({
                     sublistId: 'item',
                     fieldId: 'item',
                     line: i
                 });
+                const amount = invoiceRec.getSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'amount',
+                    line: i
+                });
+                log.debug('currentItem', currentItem)
+                log.debug('amount', amount)
+                const lineIdentifierInv = invoiceRec.getSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'custcol_rda_line_identifier',
+                    line: i
+                });
 
-                const match = linesData.find(ld => String(ld.itemId) === String(currentItem));
+                log.debug('lineIdentifierInv', lineIdentifierInv);
 
-                if (!match) {
+                const match = linesData.find(ld => 
+                    String(ld.itemId) === String(currentItem) &&
+                    String(ld.lineIdentifier) === String(lineIdentifierInv)
+                );
+
+                // const protectedItems = [38, 39, 11];
+                const protectedItems = [];
+                const isProtected = protectedItems.includes(Number(currentItem));
+
+                if (!match && !isProtected) {
                     invoiceRec.removeLine({ sublistId: 'item', line: i, ignoreRecalc: true });
                     continue;
                 }
 
-                invoiceRec.selectLine({ sublistId: 'item', line: i });
-                invoiceRec.setCurrentSublistValue({
-                    sublistId: 'item',
-                    fieldId: 'quantity',
-                    value: match.qty
-                });
-                invoiceRec.commitLine({ sublistId: 'item' });
+                if (match) {
+                    invoiceRec.selectLine({ sublistId: 'item', line: i });
+                    invoiceRec.setCurrentSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'quantity',
+                        value: match.qty
+                    });
+                    invoiceRec.commitLine({ sublistId: 'item' });
+                }
             }
+
 
             const invId = invoiceRec.save({ enableSourcing: true, ignoreMandatoryFields: false });
             log.audit(`Invoice Created`, `Invoice ID: ${invId} dari Fulfillment ID: ${fulfillmentId} / SO ID: ${soId}`);
